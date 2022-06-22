fc643b13ca586372da8d82cb8fdbc0f61d698f48
KarolineGoergen <goergenkarol@gmail.com>
2022-06-21 22:29:36 -0300

    atividade finalizada

--- controller/resources/views/cidades/edit.blade.php
+++ controller/resources/views/cidades/edit.blade.php
@@ -1,9 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+</head>
+<body>
 <h2>Alterar Cidade</h2>
 <form action="{{ route('cidades.update', $dados['id']) }}" method="POST">
    @csrf
    @method('PUT')
-   <a href="{{route('cidades.index')}}"><h4>voltar</h4></a>
-   <label>Nome: </label> <input type='text' name='nome' value='{{$dados['nome']}}'>
-   <label>Porte: </label> <input type='text' name='porte' value='{{$dados['porte']}}'>
-   <input type="submit" value="Salvar">
+   <div class="input-group mb-3">
+        <div class="input-group-prepend">
+            <span class="input-group-text" id="inputGroup-sizing-default">Nome</span>
+        </div>
+        <input type="text" class="form-control" aria-label="Exemplo do tamanho do input" aria-describedby="inputGroup-sizing-default" name='nome' value='{{$dados['nome']}}'>
+    </div>
+    <select class="form-control" name='porte'>
+      <option value="Pequeno">Pequeno</option>
+      <option value="Médio">Médio</option>
+      <option value="Grande">Grande</option>
+   </select>
+   <br>
+   <input type="submit" value="Salvar" class="btn btn-success">
+   <a href="{{route('cidades.index')}}"><h3>voltar</h3></a>
 </form>
+
+</body>
+</html>
--- controller/resources/views/cidades/index.blade.php
+++ controller/resources/views/cidades/index.blade.php
@@ -1,6 +1,16 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+</head>
+<body>
 <h2>Lista de Cidades</h2>
-<a href="{{ route('cidades.create') }}"><h4>Novo Cliente</h4></a>
-<table>
+<table class="table table-bordered table-striped">
    <thead>
        <tr>
            <th>ID</th>
@@ -16,15 +26,19 @@
                <td>{{ $item['id'] }}</td>
                <td>{{ $item['nome'] }}</td>
                <td>{{ $item['porte'] }}</td>
-               <td><a href="{{ route('cidades.edit', $item['id']) }}">editar</a></td>
+               <td><a href="{{ route('cidades.edit', $item['id']) }}">Editar</a></td>
                <td>
                    <form action="{{ route('cidades.destroy', $item['id']) }}" method="POST">
                        @csrf
                        @method('DELETE')
-                       <input type='submit' value='remover'>
+                       <input type='submit' value='remover' type="button" class="btn btn-danger">
                    </form>
                </td>
            </tr>
        @endforeach
    </tbody>
 </table>
+<a href="{{ route('cidades.create') }}"><h3>Cadastrar Cidade</h3></a>
+
+</body>
+</html>
