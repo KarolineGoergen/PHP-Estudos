98f6a6ff290ff7c50ced2ad98ef09c5409c206cd
KarolineGoergen <goergenkarol@gmail.com>
2022-09-19 11:02:50 -0300

    problema do login resolvido

--- trab_final/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ trab_final/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Providers\RouteServiceProvider;
+use App\Facades\UserPermissions;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -31,6 +32,8 @@ class AuthenticatedSessionController extends Controller
         $request->authenticate();
 
         $request->session()->regenerate();
+
+        UserPermissions::loadPermissions(Auth::user()->role_id);
 
         return redirect()->intended(RouteServiceProvider::HOME);
     }
--- trab_final/config/app.php
+++ trab_final/config/app.php
@@ -195,6 +195,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\PermissionFacadeServiceProvider::class,
+        App\Providers\CustomFacadeServiceProvider::class,
 
     ],
 
